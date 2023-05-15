d07a2d665eacf9934b05693c4e74a1ab532a9ed9
Tanmesh86 <techinndustan365@gmail.com>
2023-05-15 19:55:29 +0530

    2nd commit

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
+const path = require("path");
 app.use(cors());
 const port = process.env.PORT || 3000;
 const apiData = require("./data.json");
 app.get("/", (req, res) => {
-    res.send("Hello I am live");
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.get("/service", (req, res) => {
